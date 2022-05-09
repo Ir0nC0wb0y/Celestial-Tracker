0ae7061f7ffeba3a7c74e23376572710c360f6f3
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2022-05-09 15:25:01 -0500

    fixed WiFi pseudo code

--- Device Software/Celestial Tracker/src/GetData.cpp
+++ Device Software/Celestial Tracker/src/GetData.cpp
@@ -4,41 +4,41 @@
 
 lla WhereIsTheISS() {
   
-  String ISS_location;
+  String ISS_JSON;
   lla ISS_lla;
   
-if WiFi == connected {
-  //ISS_location = GET_Request("https://api.wheretheiss.at/v1/satellites/25544");
-  HTTPClient http;
-  http.begin("https://api.wheretheiss.at/v1/satellites/25544");
-  int httpResponseCode = http.GET();
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    ISS_location = http.getString();
-    Serial.println(ISS_location);
+  if (WiFi.status() = WL_CONNECTED) {
+    //ISS_location = GET_Request("https://api.wheretheiss.at/v1/satellites/25544");
+    HTTPClient http;
+    http.begin("https://api.wheretheiss.at/v1/satellites/25544");
+    int httpResponseCode = http.GET();
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      ISS_JSON = http.getString();
+      Serial.println(ISS_location);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end(); // Free resources
+    
+    // Do something with the data
+    JSONVar ISS_obj = JSON.parse(ISS_JSON);
+    if (JSON.typeof(ISS_obj) == "undefined") {
+      Serial.println("Parsing input failed!");
+    } else {
+      ISS_lla.latitude = atof(ISS_obj["latitude"]); 
+      ISS_lla.latitude = atof(ISS_obj["longitude"]);
+      ISS_lla.latitude = atof(ISS_obj["altitude"]); // comes in km
+      Serial.println("Parsed LLA");
+    }
+    
+  } else {
+    // If WiFi not connected, do something.
+    Serial.println("WiFi not Connected, maintaining LLA 0's");
   }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-
-  JSONVar ISS_obj = JSON.parse(ISS_location);
-  if (JSON.typeof(ISS_obj) == "undefined") {
-    Serial.println("Parsing input failed!");
-  }
-
-  
-  ISS_lla.latitude = atof(ISS_obj["latitude"]); 
-  ISS_lla.latitude = atof(ISS_obj["longitude"]);
-  ISS_lla.latitude = atof(ISS_obj["altitude"]); // comes in km
-} else {
-  ISS_lla.latitude = 0; 
-  ISS_lla.latitude = 0;
-  ISS_lla.latitude = 0; // comes in km
-}
   
 
   return ISS_lla;
