7c67041165d2a63ca02fce1a57de55d4bc7a88d8
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2022-05-09 16:41:34 -0500

    Created Move To

--- Device Software/Celestial Tracker/src/Move.h
+++ Device Software/Celestial Tracker/src/Move.h
@@ -12,17 +12,21 @@
 class Position {
   public:
     Position();
-    void AccumulateMove(float move_Az, float move_El);
+    bool AccumulateMove(float move_Az, float move_El);
+    void SetAccumulation(float min_Az, float min_El);
+    void MoveTo(float angle_Az, float angle_El);
     void MoveDirect(float move_Az, float move_El);
+    void setZeroPosition(float angleAz, float angleEl);
     int getStepsAz();
     int getStepsEl();
     int getSpeedAz();
     int getSpeedEl();
-    void setPosition(float angleAz, float angleEl);
+    int getCurrentAz();
+    int getCurrentEl();
 
   private:
-    float _angle_Az = 0;
-    float _angle_El = 0;
+    float _angle_Az = 0; // Current Az position
+    float _angle_El = 0; // Current El position
     float _angle_Az_last = 0;
     float _angle_El_last = 0;
     float _angle_Az_new = 0;
@@ -33,4 +37,8 @@ class Position {
     float _move_El = 0;
     int _speed_Az = 0;
     int _speed_El = 0;
+    float _min_Az = 0.0;
+    float _min_El = 0.0;
+    void _Calc_Steps(float move_Az, float move_El);
+    void _Calc_Speed();
 };
